66cb07b94fb55b74552599c3812d26848c0f6c0a
Roig <groig@protonmail.com>
2021-11-01 15:19:13 -0400

    prod config

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,7 @@ import Config
 # which you should run after static files are built and
 # before starting your production server.
 config :easypodcasts, EasypodcastsWeb.Endpoint,
-  url: [host: "example.com", port: 80],
+  url: [host: "roig.is-a.dev", path: "/podcasts", port: 4003],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
