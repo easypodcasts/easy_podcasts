1220df3bcebbafd21607b3c8e6ca3417f5762d4f
Roig <groig@protonmail.com>
2021-11-02 13:02:58 -0400

    remove generated code

--- lib/easypodcasts/channels.ex
+++ lib/easypodcasts/channels.ex
@@ -77,24 +77,6 @@ defmodule Easypodcasts.Channels do
   end
 
   @doc """
-  Updates a channel.
-
-  ## Examples
-
-      iex> update_channel(channel, %{field: new_value})
-      {:ok, %Channel{}}
-
-      iex> update_channel(channel, %{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
-  """
-  def update_channel(%Channel{} = channel, attrs) do
-    channel
-    |> Channel.changeset(attrs)
-    |> Repo.update()
-  end
-
-  @doc """
   Deletes a channel.
 
   ## Examples
@@ -126,19 +108,6 @@ defmodule Easypodcasts.Channels do
   alias Easypodcasts.Channels.Episode
 
   @doc """
-  Returns the list of episodes.
-
-  ## Examples
-
-      iex> list_episodes()
-      [%Episode{}, ...]
-
-  """
-  def list_episodes do
-    Repo.all(Episode)
-  end
-
-  @doc """
   Gets a single episode.
 
   Raises `Ecto.NoResultsError` if the Episode does not exist.
@@ -153,69 +122,4 @@ defmodule Easypodcasts.Channels do
 
   """
   def get_episode!(id), do: Repo.get!(Episode, id)
-
-  @doc """
-  Creates a episode.
-
-  ## Examples
-
-      iex> create_episode(%{field: value})
-      {:ok, %Episode{}}
-
-      iex> create_episode(%{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
-  """
-  def create_episode(attrs \\ %{}) do
-    %Episode{}
-    |> Episode.changeset(attrs)
-    |> Repo.insert()
-  end
-
-  @doc """
-  Updates a episode.
-
-  ## Examples
-
-      iex> update_episode(episode, %{field: new_value})
-      {:ok, %Episode{}}
-
-      iex> update_episode(episode, %{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
-  """
-  def update_episode(%Episode{} = episode, attrs) do
-    episode
-    |> Episode.changeset(attrs)
-    |> Repo.update()
-  end
-
-  @doc """
-  Deletes a episode.
-
-  ## Examples
-
-      iex> delete_episode(episode)
-      {:ok, %Episode{}}
-
-      iex> delete_episode(episode)
-      {:error, %Ecto.Changeset{}}
-
-  """
-  def delete_episode(%Episode{} = episode) do
-    Repo.delete(episode)
-  end
-
-  @doc """
-  Returns an `%Ecto.Changeset{}` for tracking episode changes.
-
-  ## Examples
-
-      iex> change_episode(episode)
-      %Ecto.Changeset{data: %Episode{}}
-
-  """
-  def change_episode(%Episode{} = episode, attrs \\ %{}) do
-    Episode.changeset(episode, attrs)
-  end
 end
