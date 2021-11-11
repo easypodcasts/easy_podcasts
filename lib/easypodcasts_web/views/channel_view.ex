82b943b007c6a4e2eb25ef4334542506a4060484
Roig <groig@protonmail.com>
2021-11-11 11:03:37 -0500

    change episode url in feed

--- lib/easypodcasts_web/views/channel_view.ex
+++ lib/easypodcasts_web/views/channel_view.ex
@@ -1,5 +1,6 @@
 defmodule EasypodcastsWeb.ChannelView do
   use EasypodcastsWeb, :view
+  alias Easypodcasts.EpisodeAudio
   require EEx
 
   EEx.function_from_file(:def, :feed, "lib/easypodcasts_web/templates/channel/feed.xml.eex", [
