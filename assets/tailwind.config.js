42155d82dbd6ef16772b74328e8534f195a421f9
Roig <groig@protonmail.com>
2021-11-20 14:00:09 -0500

    add fallback images

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -1,8 +1,17 @@
+const isProd = process.env.NODE_ENV === "production";
+
 module.exports = {
   mode: "jit",
   darkMode: "media",
   purge: ["./js/**/*.js", "../lib/*_web/**/*.*ex"],
-  theme: {},
+  theme: {
+    extend: {
+      backgroundImage: {
+        "placeholder-small": isProd ? "url('/podcasts/images/placeholder-small.webp')" : "url('/images/placeholder-small.webp')",
+        "placeholder-big": isProd ? "url('/podcasts/images/placeholder-big.webp')" : "url('/images/placeholder-big.webp')",
+      },
+    },
+  },
   variants: {},
   plugins: [require("@tailwindcss/line-clamp")],
 };
