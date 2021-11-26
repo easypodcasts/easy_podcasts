37f562ef4221fc10b9e294c6b08ecc0a14c401d2
Roig <groig@protonmail.com>
2021-11-26 18:36:13 -0500

    use the queue only for storage

---
+++ lib/easypodcasts/queue.ex
@@ -0,0 +1,53 @@
+defmodule Easypodcasts.Queue do
+  use GenServer
+  require Logger
+  alias Easypodcasts.Episodes
+
+  @name __MODULE__
+
+  def start_link(_opts) do
+    Logger.info("#{@name} starting")
+    GenServer.start_link(__MODULE__, :ok, name: @name)
+  end
+
+  def in_(episode) do
+    GenServer.call(@name, {:in, episode})
+  end
+
+  def out() do
+    GenServer.call(@name, :out)
+  end
+
+  # Internal Callbacks
+  def init(:ok) do
+    queue = :queue.new()
+    {:ok, queue, {:continue, :get_queued_episodes}}
+  end
+
+  def handle_continue(:get_queued_episodes, _state) do
+    Logger.info("#{@name} getting queued episodes from database")
+
+    {:noreply,
+     Episodes.get_queued_episodes()
+     |> :queue.from_list()}
+  end
+
+  def handle_call({:in, episode}, _from, queue) do
+    Logger.debug("#{@name} adding episode #{episode.id}")
+    queue = :queue.in(episode, queue)
+
+    {:reply, :ok, queue}
+  end
+
+  def handle_call(:out, _from, queue) do
+    Logger.debug("#{@name} extracting episode")
+
+    {episode, queue} =
+      case :queue.out(queue) do
+        {:empty, queue} -> {:empty, queue}
+        {{:value, episode}, queue} -> {episode, queue}
+      end
+
+    {:reply, episode, queue}
+  end
+end
