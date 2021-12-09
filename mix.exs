c807de139ed2a2d5c3037758503a7e649ab98ea7
Roig <groig@protonmail.com>
2021-12-09 17:22:06 -0500

    elixir 1.13

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Easypodcasts.MixProject do
     [
       app: :easypodcasts,
       version: "0.1.0",
-      elixir: "~> 1.12",
+      elixir: "~> 1.13",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
