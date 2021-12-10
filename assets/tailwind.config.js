76b5ca18b970f4ae710f48d5266ef5a50aa1a718
Roig <groig@protonmail.com>
2021-12-09 21:05:37 -0500

    upgrade to tailwind 3

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -1,17 +1,15 @@
 const isProd = process.env.NODE_ENV === "production";
 
 module.exports = {
-  mode: "jit",
-  darkMode: "media",
-  purge: ["./js/**/*.js", "../lib/*_web/**/*.*ex"],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "placeholder-small": isProd ? "url('/podcasts/images/placeholder-small.webp')" : "url('/images/placeholder-small.webp')",
-        "placeholder-big": isProd ? "url('/podcasts/images/placeholder-big.webp')" : "url('/images/placeholder-big.webp')",
-      },
+    mode: "jit",
+    content: ["./js/**/*.js", "../lib/*_web/**/*.*ex"],
+    theme: {
+        extend: {
+            backgroundImage: {
+                "placeholder-small": isProd ? "url('/podcasts/images/placeholder-small.webp')" : "url('/images/placeholder-small.webp')",
+                "placeholder-big": isProd ? "url('/podcasts/images/placeholder-big.webp')" : "url('/images/placeholder-big.webp')",
+            },
+        },
     },
-  },
-  variants: {},
-  plugins: [require("@tailwindcss/line-clamp")],
+    plugins: [require("@tailwindcss/line-clamp")],
 };
