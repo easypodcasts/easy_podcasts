40fe7b8ff4db7505c8bad7c7634aad76b259c7e6
Roig <groig@protonmail.com>
2022-02-11 14:57:25 -0500

    only show topbar for pages that take longer to load

--- assets/js/app.js
+++ assets/js/app.js
@@ -37,13 +37,13 @@ Hooks.PlayerHook = {
     this.player.unload();
   },
   getElement(el) {
-    return this.el.querySelector(el)
+    return this.el.querySelector(el);
   },
   formatTime(secs) {
     const minutes = Math.floor(secs / 60) || 0;
-    const seconds = (secs - minutes * 60) || 0;
+    const seconds = secs - minutes * 60 || 0;
 
-    return minutes + ':' + (seconds < 10 ? '0' : '') + seconds;
+    return minutes + ":" + (seconds < 10 ? "0" : "") + seconds;
   },
   setupPlayer() {
     const audioUrl = this.el.dataset.audioUrl;
@@ -59,16 +59,18 @@ Hooks.PlayerHook = {
       console.log("clicked progress");
       let rect = event.target.getBoundingClientRect();
       let x = event.clientX - rect.left;
-      let clickedValue = (x * this.player.duration()) / progressWrapper.offsetWidth;
+      let clickedValue =
+        (x * this.player.duration()) / progressWrapper.offsetWidth;
       this.player.seek(clickedValue);
-      progress.style.width = ((clickedValue / this.player.duration()) * 100 || 0) + "%";
+      progress.style.width =
+        ((clickedValue / this.player.duration()) * 100 || 0) + "%";
     };
 
     let step = () => {
       var seek = this.player.seek() || 0;
       let strCurrentTime = this.formatTime(Math.round(seek));
       currentTime.textContent = strCurrentTime;
-      progress.style.width = `${((seek / this.player.duration()) * 100 || 0)} %`;
+      progress.style.width = `${(seek / this.player.duration()) * 100 || 0} %`;
       if (this.player.playing()) {
         requestAnimationFrame(step);
       }
@@ -100,17 +102,22 @@ Hooks.PlayerHook = {
         pauseButton.classList.add("hidden");
         playButton.classList.remove("hidden");
       },
-      onseek: function() {
+      onseek: function () {
         requestAnimationFrame(step);
-      }
+      },
     });
 
     this.player.play();
   },
 };
 
-let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content");
-let liveSocket = new LiveSocket("/live", Socket, { params: { _csrf_token: csrfToken }, hooks: Hooks });
+let csrfToken = document
+  .querySelector("meta[name='csrf-token']")
+  .getAttribute("content");
+let liveSocket = new LiveSocket("/live", Socket, {
+  params: { _csrf_token: csrfToken },
+  hooks: Hooks,
+});
 
 liveSocket.socket.onError((error, transport, establishedConnections) => {
   if (transport === WebSocket && establishedConnections === 0) {
@@ -121,8 +128,20 @@ liveSocket.socket.onError((error, transport, establishedConnections) => {
 
 // Show progress bar on live navigation and form submits
 topbar.config({ barColors: { 0: "#29d" }, shadowColor: "rgba(0, 0, 0, .3)" });
-window.addEventListener("phx:page-loading-start", (info) => topbar.show());
-window.addEventListener("phx:page-loading-stop", (info) => topbar.hide());
+let topBarScheduled = undefined;
+
+topbar.config({ barColors: { 0: "#29d" }, shadowColor: "rgba(0, 0, 0, .3)" });
+
+window.addEventListener("phx:page-loading-start", (info) => {
+  if (!topBarScheduled) {
+    topBarScheduled = setTimeout(() => topbar.show(), 500);
+  }
+});
+window.addEventListener("phx:page-loading-stop", (info) => {
+  clearTimeout(topBarScheduled);
+  topBarScheduled = undefined;
+  topbar.hide();
+});
 
 // connect if there are any LiveViews on the page
 liveSocket.connect();
