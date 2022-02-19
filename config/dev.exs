c20ea11154af729d1bc3bee5aefbdd237ce99a22
Roig <groig@protonmail.com>
2022-02-18 21:21:46 -0500

    migrate to tailwind standalone

--- config/dev.exs
+++ config/dev.exs
@@ -26,13 +26,7 @@ config :easypodcasts, EasypodcastsWeb.Endpoint,
   watchers: [
     # Start the esbuild watcher by calling Esbuild.install_and_run(:default, args)
     esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]},
-    npx: [
-      "tailwindcss",
-      "--input=css/app.css",
-      "--output=../priv/static/assets/app.css",
-      "--watch",
-      cd: Path.expand("../assets", __DIR__)
-    ]
+    tailwind: {Tailwind, :install_and_run, [:default, ~w(--watch)]}
   ]
 
 # ## SSL Support
