b790b21b3a6f4f3111928d723a630a7a84c7b80c
Roig <groig@protonmail.com>
2022-02-18 21:21:46 -0500

    fix backgroud of cards

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -1,12 +1,10 @@
-const isProd = process.env.NODE_ENV === "production";
-
 module.exports = {
     content: ["./js/**/*.js", "../lib/*_web/**/*.*ex"],
     theme: {
         extend: {
             backgroundImage: {
-                "placeholder-small": isProd ? "url('/podcasts/images/placeholder-small.webp')" : "url('/images/placeholder-small.webp')",
-                "placeholder-big": isProd ? "url('/podcasts/images/placeholder-big.webp')" : "url('/images/placeholder-big.webp')",
+                "placeholder-small": "url('/images/placeholder-small.webp')",
+                "placeholder-big": "url('/images/placeholder-big.webp')",
             },
         },
     },
