ff4d514bdb77ea4ce2aeabc178eee2d32590bbdb
Roig <groig@protonmail.com>
2022-04-07 11:19:12 -0400

    update dependencies. migrate to Phoenix.LiveView.HTMLFormatter

--- .formatter.exs
+++ .formatter.exs
@@ -1,5 +1,5 @@
 [
-  plugins: [HeexFormatter],
+  plugins: [Phoenix.LiveView.HTMLFormatter],
   import_deps: [:ecto, :phoenix],
   inputs: ["*.{heex,ex,exs}", "priv/*/seeds.exs", "{config,lib,test}/**/*.{heex,ex,exs}"],
   subdirectories: ["priv/*/migrations"],
