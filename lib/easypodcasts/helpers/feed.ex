4f29be76a4eadf8c003f9d8fe40ef5b8e2e17d49
Roig <groig@protonmail.com>
2022-08-06 11:06:25 -0400

    simple retry for proxified urls

--- lib/easypodcasts/helpers/feed.ex
+++ lib/easypodcasts/helpers/feed.ex
@@ -5,8 +5,9 @@ defmodule Easypodcasts.Helpers.Feed do
   @feed_parser Path.expand("./priv/bin/feed-parser")
   require Logger
 
-  def get_feed_data(url) do
-    url = proxify(url)
+  def get_feed_data(original_url, is_retry \\ false) do
+    url = if is_retry, do: original_url, else: proxify(original_url)
+
     Logger.info("Fetching #{url}")
 
     with {json_string, 0} <- System.cmd(@feed_parser, ["-feed.url", url]),
@@ -14,6 +15,12 @@ defmodule Easypodcasts.Helpers.Feed do
       Logger.info("Feed for #{url} fetched succesfully")
       {:ok, feed}
     else
+      {"Failed to detect feed type\n", 1} ->
+        if !is_retry, do: get_feed_data(original_url, true), else: proxify(original_url)
+
+        Logger.info("Failed to get and parse feed for #{original_url} with error #{inspect({"Failed to detect feed type\n", 1})}")
+        {:error, "Failed to get and parse feed"}
+
       error ->
         Logger.info("Failed to get and parse feed for #{url} with error #{inspect(error)}")
         {:error, "Failed to get and parse feed"}
