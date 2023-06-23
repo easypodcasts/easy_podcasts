dc1b0a89d02eb1e4042bb8f7d1309ce993e09dbb
Roig <groig@protonmail.com>
2023-06-23 15:28:45 -0400

    update dependencies

--- mix.exs
+++ mix.exs
@@ -30,6 +30,7 @@ defmodule Easypodcasts.MixProject do
   # Specifies your project dependencies.
   #
   # Type `mix help deps` for examples and options.
+
   defp deps do
     [
       {:phoenix, "~> 1.7.0"},
